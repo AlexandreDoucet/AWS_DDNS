f16a1f14d70ddd3f6163f102d973373cca7c6f1a
AlexandreDoucet <alexandre.doucet6@gmail.com>
2023-09-04 21:04:30 -0300

    Made the code use an environement variable for the IAM information istead of hardcode

--- source/main.py
+++ source/main.py
@@ -3,7 +3,7 @@ import requests
 import socket
 import datetime
 import subprocess
-
+import os
 
 #subprocess.call(['aws','configure'])
 
@@ -15,18 +15,53 @@ def check_internet_connection():
     except subprocess.CalledProcessError:
         return False
 
+def get_aws_profile_info():
+	aws_user = os.environ.get("IAM_USER")
+	aws_key = os.environ.get("IAM_KEY")
 
+	return aws_user, aws_key
+
+def create_aws_profile(aws_user, aws_key):
+	try:
+		# Use subprocess to execute the aws configure set commands.
+		subprocess.run(["aws", "configure", "set", "aws_access_key_id", aws_user, "--profile", "myprofile"])
+		subprocess.run(["aws", "configure", "set", "aws_secret_access_key", aws_key, "--profile", "myprofile"])
+		print("AWS profile 'myprofile' created successfully.")
+	except Exception as e:
+		print(f"Failed to create AWS profile: {str(e)}")
+
+
+time_to_wait = 20
+alreadyPrinted = False
 while True:
-    timeToWait = 10
-    if check_internet_connection():
-        print("Internet connection is available.")
-        break
-    else:
-        print("No internet connection. Waiting for "+str(timeToWait)+" seconds...")
-        time.sleep(timeToWait)
 
+	if check_internet_connection() :
+		print("Internet connection is available.")
+		break
+	else:
+		if not alreadyPrinted:
+			print("No internet available\r\t\t\t\r")
+			alreadyPrinted = True
+		time.sleep(time_to_wait)
 
+try:
+	subprocess.check_call(['aws', 'configure', 'get', 'aws_access_key_id'])
+	print("AWS CLI is already configured with IAM credentials.")
+except subprocess.CalledProcessError:
+	print("AWS CLI is not configured. Running 'aws configure'...")
 
+	aws_user, aws_key = get_aws_profile_info()
+
+	if aws_user and aws_key:
+		print("Using AWS credentials from environment variables.")
+		create_aws_profile(aws_user, aws_key)
+	else:
+		print("AWS_USER and/or AWS_KEY environment variables not set. Please set them or run 'aws configure' manually.")
+		print("AWS CLI is not configured. Running 'aws configure'...")
+		subprocess.call(['aws', 'configure'])
+	
+
+'''
 # Check if AWS CLI is already configured
 try:
     subprocess.check_call(['aws', 'configure', 'get', 'aws_access_key_id'])
@@ -34,6 +69,8 @@ try:
 except subprocess.CalledProcessError:
     print("AWS CLI is not configured. Running 'aws configure'...")
     subprocess.call(['aws', 'configure'])
+'''
+
 
 print("This prorgram will querry a service and an DNS name, compare them and return wether the record should be updated")
 print("Started : " + str(datetime.datetime.today()) + "\n~~~~~~~~~~~")
